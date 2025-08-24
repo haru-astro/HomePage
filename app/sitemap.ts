aedb1137676d35e95bad90857849a8169c87bdfd
{haru-astro} <{hayakawa14959@gmail.com}>
2025-08-24 11:20:42 +0900

    change sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,3 +1,17 @@
 import { getBlogPosts } from 'app/blog/utils'
 
 export const baseUrl = 'https://haruhayakawa.vercel.app'
+
+export default async function sitemap() {
+  let blogs = getBlogPosts().map((post) => ({
+    url: `${baseUrl}/blog/${post.slug}`,
+    lastModified: post.metadata.publishedAt,
+  }))
+
+  let routes = ['', '/aboutme', '/activity','/outreach', '/blog'].map((route) => ({
+    url: `${baseUrl}${route}`,
+    lastModified: new Date().toISOString().split('T')[0],
+  }))
+
+  return [...routes, ...blogs]
+}
